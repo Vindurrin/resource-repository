8c4c9e18b82b274dc23713bc608e1408572d098d
mattm119 <47251148+mattm119@users.noreply.github.com>
2019-04-29 08:37:30 -0400

    Add files via upload

--- spring-app/src/main/java/net/guides/springboot2/springboot2jpacrudexample/repository/TeamRepository.java
+++ spring-app/src/main/java/net/guides/springboot2/springboot2jpacrudexample/repository/TeamRepository.java
@@ -3,11 +3,9 @@ package net.guides.springboot2.springboot2jpacrudexample.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import net.guides.springboot2.springboot2jpacrudexample.model.Resource;
+import net.guides.springboot2.springboot2jpacrudexample.model.Team;
 
 @Repository
-public interface ResourceRepository extends JpaRepository<Resource, Long>{
-	
-		
+public interface TeamRepository extends JpaRepository<Team, Long>{
 
 }
