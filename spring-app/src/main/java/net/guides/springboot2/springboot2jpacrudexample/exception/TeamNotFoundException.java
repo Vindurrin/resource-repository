30f87cb77644c7eb8c9567ad0e9f2f696e1226ad
mattm119 <47251148+mattm119@users.noreply.github.com>
2019-04-29 08:36:35 -0400

    Add files via upload

---
+++ spring-app/src/main/java/net/guides/springboot2/springboot2jpacrudexample/exception/TeamNotFoundException.java
@@ -0,0 +1,14 @@
+package net.guides.springboot2.springboot2jpacrudexample.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class TeamNotFoundException extends Exception{
+
+    private static final long serialVersionUID = 1L;
+
+    public TeamNotFoundException(String message){
+        super(message);
+    }
+}
