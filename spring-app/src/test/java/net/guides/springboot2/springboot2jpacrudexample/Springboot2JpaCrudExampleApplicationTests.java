f2b684fe4ded3703b2f1db60856ebbb1f5010576
Matthew Burt <vindurrin@outlook.com>
2019-05-01 11:02:55 -0400

    Update Springboot2JpaCrudExampleApplicationTests.java

--- spring-app/src/test/java/net/guides/springboot2/springboot2jpacrudexample/Springboot2JpaCrudExampleApplicationTests.java
+++ spring-app/src/test/java/net/guides/springboot2/springboot2jpacrudexample/Springboot2JpaCrudExampleApplicationTests.java
@@ -1,7 +1,13 @@
 package net.guides.springboot2.springboot2jpacrudexample;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+import java.sql.Connection;
+import java.util.Optional;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +16,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import net.guides.springboot2.springboot2jpacrudexample.controller.ResourceController;
 import net.guides.springboot2.springboot2jpacrudexample.controller.TeamController;
+import net.guides.springboot2.springboot2jpacrudexample.model.Resource;
+import net.guides.springboot2.springboot2jpacrudexample.repository.ResourceRepository;
+import net.guides.springboot2.springboot2jpacrudexample.repository.TeamRepository;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -21,10 +30,28 @@ public class Springboot2JpaCrudExampleApplicationTests {
 	@Autowired
 	private TeamController teamController;
 
+	@Autowired
+	private ResourceRepository resourceRepository;
+
+	@Autowired
+	private TeamRepository teamRepository;
+
+	@Before
+	public void setUp() {
+		System.out.println("Set Up");
+	}
+
+	@After
+	public void tearDown() {
+		System.out.println("Tear Down");
+	}
+
 	@Test
 	public void contextLoads() throws Exception {
 		assertNotNull(resourceController);
 		assertNotNull(teamController);
 	}
+
+	
 
 }
